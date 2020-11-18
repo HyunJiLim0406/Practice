3aa3e2720efb03f24ada3fa4bece3548f68b083e
iw0406 <hyunjilim98@gmail.com>
2020-11-18 19:46:50 +0900

    3.3

---
+++ deep-learning/step_sigmoid.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+def step_function(x):
+    return np.array(x>0, dtype=np.int)
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+x = np.arange(-5.0, 5.0, 0.1)
+y1 = step_function(x)
+y2 = sigmoid(x)
+plt.plot(x, y1, linestyle="--", label="step")
+plt.plot(x, y2, label="sigmoid")
+plt.ylim(-0.1, 1.1)
+plt.title("step & sigmoid")
+plt.legend()
+plt.show()
